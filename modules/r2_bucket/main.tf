66d5a5083bf98ea43999fca3e1a2e7d10acaebfb
karthik murali <karthikmurali123@gmail.com>
2025-03-08 13:33:41 +0530

    seven

--- modules/r2_bucket/main.tf
+++ modules/r2_bucket/main.tf
@@ -4,7 +4,7 @@ resource "cloudflare_r2_bucket" "example_r2_bucket" {
   location = var.r2_location
 }
 
-resource "cloudflare_r2_bucket" "example_r2_bucket" {
+resource "cloudflare_r2_bucket" "example_r2_bucket2" {
   account_id = var.cloudflare_account_id
   name = "tf-bucket2"
   location = var.r2_location
