c33f5cf9aecc699aa1789f08a9a225749ceeedb6
karthik murali <karthikmurali123@gmail.com>
2025-03-08 12:26:55 +0530

    second commit

--- modules/r2_bucket/main.tf
+++ modules/r2_bucket/main.tf
@@ -3,3 +3,9 @@ resource "cloudflare_r2_bucket" "example_r2_bucket" {
   name = "tf-bucket"
   location = var.r2_location
 }
+
+resource "cloudflare_r2_bucket" "example_r2_bucket" {
+  account_id = var.cloudflare_account_id
+  name = "tf-bucket2"
+  location = var.r2_location
+}
