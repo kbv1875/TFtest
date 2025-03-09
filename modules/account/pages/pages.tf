546d5da1327b3359336791b0eb23215190497005
karthik murali <karthikmurali123@gmail.com>
2025-03-10 00:29:01 +0530

    ww1

--- modules/account/pages/pages.tf
+++ modules/account/pages/pages.tf
@@ -9,5 +9,4 @@ resource "cloudflare_pages_domain" "karthik_kbv1875_com" {
     account_id = var.cloudflare_account_id
     project_name = cloudflare_pages_project.kbv1875.name
     domain = "karthik.kbv1875.com"
-
 }
