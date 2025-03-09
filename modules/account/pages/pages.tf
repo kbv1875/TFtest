effa995aa31fe61a94b5d0a1b462b5ea559033dc
karthik murali <karthikmurali123@gmail.com>
2025-03-10 00:04:01 +0530

    wow8

--- modules/account/pages/pages.tf
+++ modules/account/pages/pages.tf
@@ -4,3 +4,10 @@ resource "cloudflare_pages_project" "kbv1875" {
     production_branch = "main"
   
 }
+
+resource "cloudflare_pages_domain" "karthik_kbv1875_com" {
+    account_id = var.cloudflare_account_id
+    project_name = cloudflare_pages_project.kbv1875.name
+    domain = "karthik.kbv1875.com"
+
+}
