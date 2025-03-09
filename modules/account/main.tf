a91e5f0e1070fb4b3b82b65641de2da12ff7de6e
karthik murali <karthikmurali123@gmail.com>
2025-03-09 23:51:25 +0530

    wow4

--- modules/account/main.tf
+++ modules/account/main.tf
@@ -1,7 +1,14 @@
 module "r2_bucket" {
-  source = "./modules/r2_bucket"
+  source = "./r2_bucket"
   cloudflare_api_token = var.cloudflare_api_token
   cloudflare_account_id = var.cloudflare_account_id
   r2_location = var.r2_location
+}
+
+module "pages" {
+  source = "./pages"
+  cloudflare_api_token = var.cloudflare_api_token
+  cloudflare_account_id = var.cloudflare_account_id
+  
 }
   
